b03ac46b817ccd82957339d3f6f2380cbf92e49e
James Nguyen <jamesminhanh@gmail.com>
2019-09-17 20:05:56 -0700

    Updates

---
+++ component---src-pages-404-js-0435efeb96f15c361e79.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{243:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(241),o=n(240);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0435efeb96f15c361e79.js.map
---
+++ component---src-pages-page-2-js-ac850f879ec37b7255b8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{244:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(145),c=a(241),r=a(240);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-ac850f879ec37b7255b8.js.map
